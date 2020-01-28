73ca70077cb4e0e0a7576cb93d803520b99028da
unknown <newbiecraft@gmail.com>
2020-01-28 16:26:04 +0300

    my modify

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-KLOND90
+KLOND90  sss
         </p>
         <a
           className="App-link"
